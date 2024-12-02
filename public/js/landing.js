9a0cfdc7fd6468f8ebe76c4060caf764588f6453
Ajay Ambule <ajayambule2003@gmail.com>
2024-12-03 00:41:28 +0530

    Landing page updated:

--- public/js/landing.js
+++ public/js/landing.js
@@ -3,11 +3,11 @@
     const e = document.documentElement;
     if ((e.classList.remove('no-js'), e.classList.add('js'), document.body.classList.contains('has-animations'))) {
         (window.sr = ScrollReveal()).reveal('.feature, .pricing-table-inner', {
-            duration: 600,
-            distance: '20px',
-            easing: 'cubic-bezier(0.5, -0.01, 0, 1.005)',
-            origin: 'bottom',
-            interval: 100,
+            duration: 600, 
+            distance: '20px', 
+            easing: 'cubic-bezier(0.5, -0.01, 0, 1.005)', 
+            origin: 'bottom', 
+            interval: 100, 
         }),
             e.classList.add('anime-ready'),
             anime 
