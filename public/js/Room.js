5f839d44c1175378450a629486398eeb70968099
Ajay Ambule <ajayambule2003@gmail.com>
2024-12-09 22:01:09 +0530

    Room File Updated:

--- public/js/Room.js
+++ public/js/Room.js
@@ -3,17 +3,17 @@
 if (location.href.substr(0, 5) !== 'https') location.href = 'https' + location.href.substr(4, location.href.length - 4);
 
 /**
- * MiroTalk SFU - Room component
+ * MiroTalk SFU - Room component 
  *
  * @link    GitHub: https://github.com/miroslavpejic85/mirotalksfu
  * @link    Official Live demo: https://sfu.mirotalk.com
- * @license For open source use: AGPLv3
+ * @license For open source use: AGPLv3 
  * @license For commercial or closed source, contact us at license.mirotalk@gmail.com or purchase directly via CodeCanyon
  * @license CodeCanyon: https://codecanyon.net/item/mirotalk-sfu-webrtc-realtime-video-conferences/40769970
  * @author  AS Developers - sushilkumar.yadav.9545@gmail.com
- * @version 1.3.52
- *
- */
+ * @version 1.3.52 
+ * 
+ */ 
 
 console.log('Window Location', window.location);
 
